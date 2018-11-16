18973953bdf5f936c3cc93df165c15062a780b6a
Gavin <gavin.kelly@crick.ac.uk>
2018-11-16 14:36:20 +0000

    Add tidy approach to loops

--- analyse.r
+++ analyse.r
@@ -25,6 +25,53 @@ params <- params_init(
   lfcThreshold=0
 )
 
+## *** Useful Functions
+## Only use certain samples
+applySubset <- function(dds, subs) {
+  tmp <- dds[,subs]
+  colData(tmp) <- droplevels(colData(tmp))
+  tmp
+}
+ddsList <-map(datasetList, ~ applySubset(dds_all,.))
+## Do likelihood ratio test, and classify in order of effect size
+## Doesn't work for interactions, obviously
+applyLRT <- function(dds, mdl) {
+  design(dds) <- mdl$full
+  dds <- DESeq(dds, test="LRT", full=mdl$full, reduced=mdl$reduced)
+  cols <- resultsNames(dds)
+  inBoth <- intersect(attr(terms(mdl$full), "term.labels"),
+                     attr(terms(mdl$reduced), "term.labels"))
+  testTerm <- setdiff(attr(terms(mdl$full), "term.labels"),
+                     attr(terms(mdl$reduced), "term.labels"))
+  if (length(inBoth)) {
+    cols <- cols[!grepl(inBoth, cols)]
+  }
+  toOrd <- mcols(dds)[cols]
+  toOrd[,1] <- 0
+  ords <-  apply(toOrd, 1, order)
+  str <- matrix(levels(dds[[testTerm]])[ords], nrow=nrow(ords), ncol=ncol(ords))
+  mcols(dds)$class <- apply(str, 2, paste, collapse="<")
+  dds
+}
+## apply contrast, and transfer across interesting mcols from the dds
+applyResults <- function(dds, ...) {
+  r <- results(dds, ...)
+  r$symbol <- mcols(dds)$symbol
+  if ("LRTPvalue" %in% names(mcols(dds))) {
+    r$class <- mcols(dds)$class
+    r$class[is.na(r$padj) | is.na(r$pvalue) | r$baseMean==0] <- NA
+    r$log2FoldChange <- NULL
+    r$lfcSE <- NULL
+  }  else {
+    r$class <- ifelse(r$log2FoldChange >0, "Up", "Down")
+  }
+  ind <- which(r$padj<metadata(r)$alpha)
+  r$class[ind] <- paste0(r$class[ind], "*")
+  r$class[is.na(r$pvalue)] <- "Outlier"
+  r$class[is.na(r$padj)] <- "Low Count"
+  r$class[r$baseMean==0] <- "Zero Count"
+  r
+}
 
 set.seed(seed=params()$seed)
 dirs <- derivedDirs(publish=NA,
@@ -156,6 +203,21 @@ vDevice(txt, "Normalised_counts", nrc)
 summaries <- list()
 res <- list()
 
+## *** New approach
+## *** LRT template
+fitted_list <- list(dds=ddsList, mdl=modelList) %>%
+  cross %>%
+  map(lift(applyLRT))
+names(fitted_list) <-list(names(ddsList), names(modelList)) %>%
+  cross %>%
+  map(paste, collapse="_")
+res[[analysis]] <- lapply(fitted_list, applyResults, alpha=params("alpha"))
+## *** Wald template
+contrSpecific <- list()
+res[[analysis]] <- lapply(contrSpecific,  function(x) applyResults(dds, contrast=x, alpha=params("alpha")))
+
+
+
 ## *** First analysis approach
 analysis <- params("analysis" = "Across_time")
 mdlList <- list(dat=list(AP=~AP, VD=~VD, D3=~D3),
@@ -209,32 +271,124 @@ dev.off()
 
 ## ** Generate results spreadsheet
 
+################################################################
+#### Enrichment Analysis
+################################################################
+vDevice(pdf, file="Enrichment Plots")
+genes <- lapply(res, function(x) lapply(x, function(y) na.omit(entrez[row.names(y)[grepl("\\*", y$class)]])))
+genes <- flatten(genes)
+## Reactome
+fit <- compareCluster(genes, fun="enrichPathway", organism="mouse", universe=na.omit(entrez))
+pl <- dotplot(fit, showCategory=25)
+pl + theme(axis.text.x = element_text(angle = 45, hjust = 1, size=6),
+           axis.text.y = element_text(size=8))
+## GO
+fit<- compareCluster(genes, OrgDb = org.Mm.eg.db, universe=na.omit(entrez))
+df <- fortify(fit)
+levels(df$Description) <- gsub("RNA polymerase II", "RNAPii", levels(df$Description))
+levels(df$Description) <- gsub(".*,","", levels(df$Description))
+ggplot(df, aes(x = Cluster, y = Description, size = GeneRatio,  color = p.adjust)) +
+  geom_point() + scale_color_gradient(low = "red",  high = "blue") +
+  ylab("") + ggtitle("GO Enrichment") + theme_dose(6) +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1, size=6),
+        axis.text.y = element_text(angle=45, size=8))
+dev.off()
+
+################################################################
+#### Results Heatmaps
+################################################################
+
+
+wt_heatmap_dat <- assay(vst(dds_all))
+wt_heatmap_cdat <- colData(dds_all)
+row.names(wt_heatmap_cdat) <- wt_heatmap_cdat$name
+colnames(wt_heatmap_dat) <- wt_heatmap_cdat$name
+
+
+merge_hclust <- function(hclist) {
+    d <- as.dendrogram(hclist[[1]])
+    for (i in 2:length(hclist)) {
+        d <- merge(d, as.dendrogram(hclist[[i]]))
+    }
+    as.hclust(d)
+}
+
+
+vDevice(pdf, file="Heatmap of results")
+for (r in names(res)) {
+  for (i in names(res[[r]])) {
+    wt_heatmap_rdat <- data.frame(class=res[[r]][[i]]$class, row.names=row.names(wt_heatmap_dat))
+    
+    gene_ind <- grep("\\*", res[[r]][[i]]$class)
+#    max_vst_per_gene <- apply(wt_heatmap_dat[gene_ind,], 1, max)
+#    gene_ind <- gene_ind[max_vst_per_gene>quantile(max_vst_per_gene, .8)]
+    group_sizes <- table(res[[r]][[i]]$class[gene_ind]) %>% sort(decreasing=TRUE)
+    group_size <- group_sizes[res[[r]][[i]]$class[gene_ind]]
+    gene_ind <- gene_ind[order(group_size, decreasing=TRUE)]
+    gene_class <- factor(res[[r]][[i]]$class[gene_ind], levels=names(group_sizes))
+    if (length(group_sizes)>6) {
+      levels(gene_class)[cumsum(group_sizes)/sum(group_sizes) > 0.8] <- "Other*"
+    }
+    #  biggest_class <- sub("\\*","",res[[r]][[i]]$class[gene_ind][1])
+    biggest_class <- sub("\\*", "",  levels(gene_class)[1])
+    if (grepl("<", biggest_class)) {
+      sample_order <- strsplit(biggest_class, "<")[[1]]
+      wt_heatmap_cdat$tissue <- factor(as.character(wt_heatmap_cdat$tissue), levels=sample_order)
+      sample_ind <- order(match(wt_heatmap_cdat$tissue, sample_order), wt_heatmap_cdat$experiment)
+    } else {
+      sample_ind <- TRUE
+    }
+
+    plList <- tapply(gene_ind, gene_class, function(x)
+      pheatmap(wt_heatmap_dat[x,sample_ind,drop=FALSE],  cluster_cols=FALSE, silent=TRUE)$tree_row)
+    
+    row_hclust <- merge_hclust(plList)
+    pl <- pheatmap(wt_heatmap_dat[gene_ind,sample_ind],
+                   annotation_col=as.data.frame(wt_heatmap_cdat)[sample_ind, c( "tissue"),drop=FALSE],
+                   #                                  annotation_row=wt_heatmap_rdat[gene_ind,,drop=FALSE],
+                   scale="row",
+                   cluster_rows=row_hclust, # cluster_cols=!(grepl("<", biggest_class)),
+                   show_rownames=FALSE, show_colnames=FALSE,
+                   silent=TRUE,
+                   main=i)$gtable
+    ind <- pl$layout$name!="row_tree"
+    pl$layout <- pl$layout[ind, , drop = FALSE]
+    pl$grobs <- pl$grobs[ind]
+    pl <- gtable::gtable_trim(pl)
+    grid::grid.draw(pl)
+    grid::grid.newpage()
+  }
+}
+dev.off()
 
 ## *** Output filtered results
-vDevice(saveRDS, "sessionInfo.rds", sessionInfo())
 si <- session_info()
 for (analysis in names(res)) {
-  xl <- vDevice(file=paste0(analysis, ".xlsx"))
+  summaries <- do.call(cbind, lapply(res[[analysis]], function(x) table(x$class)))
+  colnames(summaries) <- names(res[[analysis]])
+  ind <- rownames(summaries) %in% c("Low Count", "Zero Count", "Outlier")
+  summaries <- rbind(summaries[ind,,drop=FALSE], summaries[!ind,,drop=FALSE])
   dframe <- t(as.data.frame(params()))
-  write.xlsx(as.data.frame(dframe), file=xl, sheetName="Parameters", col.names=FALSE)
-  #dframe <- do.call(rbind, lapply(ddsList, compose(as.data.frame, colData)))
-  dframe <- do.call(rbind, imap(ddsList, ~cbind(Dataset=.y, as.data.frame(colData(.x)))))
+  fname <- vDevice(write.xlsx, glue("differential_genelists_{analysis}.xls"),
+                  x=as.data.frame(dframe), sheetName="Parameters", col.names=FALSE)
   ## Design
-  write.xlsx(dframe, file=xl, sheetName="Design", col.names=TRUE,row.names=FALSE,  append=TRUE)
-  dframe <- data.frame(summaries[[analysis]], check.names=FALSE)
-  write.xlsx(dframe, file=xl, sheetName="Summary", col.names=TRUE,row.names=TRUE,  append=TRUE)
+  write.xlsx(colData(dds_all), file=fname, sheetName="Design", col.names=TRUE,row.names=FALSE,  append=TRUE)
+  write.xlsx(summaries, file=fname, sheetName="Class Sizes", col.names=TRUE,row.names=TRUE,  append=TRUE)
   ## Differential gene-lists
   for (i in names(res[[analysis]])) {
-    dframe <- subset(res[[analysis]][[i]], padj<params()$alpha)#[c("stat", "pvalue", "padj",   "symbol", "group")]
-    dframe <- dframe[order(dframe$stat),]
-    write.xlsx2(dframe, file=xl, sheetName=i, append=TRUE)
+    dframe <- as.data.frame(res[[analysis]][[i]]) %>%
+      rownames_to_column("id") %>%
+      filter(padj<params()$alpha) %>%
+      arrange(desc(stat))
+    write.xlsx2(dframe, file=fname, sheetName=i, append=TRUE, row.names=FALSE)
   }
   write.xlsx2(as.data.frame(si$packages), file=xl, sheetName="R Packages", row.names=FALSE, append=TRUE)
   write.xlsx2(data.frame(setting = names(si$platform), value = unlist(si$platform), stringsAsFactors = FALSE), file=xl, sheetName="R Settings", row.names=FALSE, append=TRUE)
 }
+vDevice(saveRDS, "sessionInfo.rds", sessionInfo())
 
 for (i in names(res)) {
   for (j in names(res[[i]])) { 
-    vDevice(txt, file=paste(i,j, sep="-"), x=as.data.frame(res[[i]][[j]]) %>% dplyr::select(stat, symbol, group, tpm5))
+    vDevice(txt, file=paste(i,j, sep="-"), x=as.data.frame(res[[i]][[j]]) %>% dplyr::select(stat, symbol, class))
   }
 }
