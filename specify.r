f67b31d8b0e7433aeaa5d3fbf6a52bf1cffc36d7
Gavin Kelly <gavin.kelly@crick.ac.uk>
2020-10-09 11:10:16 +0100

    Add analysis-specification prototype

---
+++ specify.r
@@ -0,0 +1,24 @@
+list(
+  sample_sets =  list(
+    all = ~ TRUE,
+    no_cancer = ~ fraction != tumour
+  ),
+  
+  models = list(
+    "Batch-corrected" = list(
+      design = ~ fraction + pool,
+      comparison = mult_comp(pairwise ~ fraction)
+    ),
+    "Naive" = list(
+      design = ~ fraction,
+      comparison = mult_comp(pairwise ~ fraction)
+    )
+  ),
+  
+  plot_scaler = function(y) {
+    group_by(pool) %>%
+      mutate(y =y / mean(y[fraction=="Control"]) ) %>%
+      ungroup()
+  }
+)
+
