afe1dde73a2269aeea635a833ced1b3f69c09810
Gavin Kelly <gavin.kelly@crick.ac.uk>
2021-01-29 15:35:55 +0000

    Skeleton scripts to become more generic
    00_init cleaned up a bit
    specify.r uses consistent plurals as list etc.

---
+++ 00_init.r
@@ -0,0 +1,56 @@
+library(readxl)
+library(tidyverse)
+library(tximport)
+library(DESeq2)
+devtools::load_all(".")
+
+sample_data <- readxl::read_excel(system.file("extdata/{{metadata_file}}.xlsx", package="babsrnaseq"), sheet=1)
+
+sample_sheet <- sample_data %>%
+  dplyr::mutate(
+           rsem = file.path(system.file("extdata/{{rsem}}", package="babsRNASeq"),
+                            paste0(LIMSID, "_R1.genes.results")) # that annoying nfcore '_R1' suffix may disappear
+         )
+
+sample_sheet[sapply(sample_sheet, is.character)] <- lapply(sample_sheet[sapply(sample_sheet, is.character)], 
+                                       as.factor)
+
+txi <- tximport(as.character(sample_sheet$rsem), type="rsem")
+txi$length[txi$length==0] <- 1
+rsem_dds <- DESeqDataSetFromTximport(txi, sample_sheet, ~ 1) # ok to have fixed trivial design - it'll get swapped out
+ind <- rowSums(counts(rsem_dds)!=0) > 0
+rsem_dds <- rsem_dds[ind,]
+
+metadata(rsem_dds)$labels <- c("{{labels}}") # which columns of colData are biologically interesting - a vector of characters
+
+txi[c("abundance", "counts", "length")] <- map(txi[c("abundance", "counts", "length")], ~ .[ind,])
+
+
+organism <- list(genus   = "{{genus}}",
+                 species = "{{species}}")
+organism$Gs <- paste0(toupper(substr(organism$genus, 1, 1)),
+                     tolower(substr(organism$species, 1, 1)))
+organism$org <-  unique(grep(paste0("org\\.",organism$Gs),row.names(installed.packages()),  value=TRUE))
+if (length(organism$org)!=1) {
+  stop(paste("Can't find annotation for", organism$genus, organism$species))
+}
+metadata(rsem_dds)$organism <- organism
+
+library(organism$org, character.only=TRUE)
+
+mcols(rsem_dds)$symbol <- mapIds(
+  eval(parse(text = organism$org)), 
+  keys=row.names(rsem_dds),
+  column="SYMBOL",
+  keytype="ENSEMBL",
+  multiVals="first")[row.names(rsem_dds)]
+
+mcols(rsem_dds)$entrez <- mapIds(
+  eval(parse(text = organism$org)),
+  keys=row.names(rsem_dds),
+  column="ENTREZID",
+  keytype="ENSEMBL",
+  multiVals="first")[row.names(rsem_dds)]
+
+usethis::use_data(rsem_dds, overwrite=TRUE)
+
--- specify.r
+++ specify.r
@@ -1,57 +1,59 @@
-list(
-  sample_sets =  list(
-    all = TRUE,
-    no_cancer = fraction != "tumour"
-  ),
-  
-  models = list(
-    "Batch-corrected" = list(
-      design = ~ fraction + pool,
-      comparison = mult_comp(pairwise ~ fraction)
+specification(
+  sample_sets =  list( # Different combinations of samples
+    all = sample_set(  # 'all' corresponds to all samples going into the dds object together
+      subset=TRUE,     # if a sample evaluate's to TRUE then include it
+      models=list(                                # We may have one or more design formulae
+            "Batch-corrected" = model(            # specify each as
+              design = ~ fraction + pool,         # a design formula
+              comparisons = list(                 # and a list of comparisons (plurals are always lists)
+                mult_comp(pairwise ~ fraction)    # all pairs of fractions are compared
+              )
+            )
+      )
     ),
-    "Naive" = list(
-      design = ~ fraction,
-      comparison = mult_comp(pairwise ~ fraction)
+    no_cancer = sample_set(                   # Another grouping of samples
+      subset = fraction != "tumour",           # this time, drop 'tumour'-fraction samples
+      models = list(
+        "Batch-corrected" = model(            # etc
+          design = ~ fraction + pool,
+          comparisons = list(
+            mult_comp(pairwise ~ fraction)
+          )
+        ),
+        "Naive" = model(
+          design = ~ fraction,                # a different model, so adjusting for different terms
+          comparisons = list(
+            mult_comp(pairwise ~ fraction)
+          )
+        )
+      )
     )
   ),
-  
-  plot_scaler = function(y) {
+  plot_scaler = function(y) { ## not currently called so no need to change
     group_by(pool) %>%
       mutate(y =y / mean(y[fraction=="Control"]) ) %>%
       ungroup()
   }
 )
 
-#### Default if this file is missing, is
-## 
-## list(
-##   sample_sets = list(all=TRUE),
-##   models=list(
-##     "Naive" = list(
-##       design = ~ covariate, # last column of design file
-##       comparison = mult_comp(pairwise ~ covariate)
-##     )
-##   ),
-##   plot_scale = function(y) {
-##     y/mean(y)
-##   }
-## )
 
 #### Example 'comparison's for more complex design
-##
-## "By Hand" = list(
+## model(
 ##   design = ~ fraction + pool, 
-##   comparison = list(                          # can be a list
-##      "f1 v f2" = list("fraction", "f2","f1"), # of DESeq2::results(contrast=e.g.
-##      "fraction" = ~ pool,                     # or LRT full=design, reduced=this
-##      mult_comp(pairwise ~ fraction | pool)     # and this will get expanded out to all pairwise
-## ),
-## "Complex" = list(
-##   design = ~ fraction * pool,                               # No replicates, but just imagine if there were
-##   comparison = list(
-##    mult_comp(pairwise ~ fraction),                          # average across pools
-##    mult_comp(pairwise ~ fraction | pool),                   # stratify over pools
-##    mult_comp(trt.vs.ctrl ~fraction | pool, ref="tumour")    # All fraction_x vs tumour, per pool.  This would break on the 'no_cancer' dataset
-##    mult_comp(pairwise ~ fraction * pool, interaction=TRUE)  # which genes have pool-dependent response to fraction.  All pairwise interactions.
+##   comparisons = list(                          # can be a list
+##     "f1 v f2" = list("fraction", "f2","f1"), # of DESeq2::results(contrast=e.g.
+##     "fraction" = ~ pool,                     # or LRT full=design, reduced=this
+##     mult_comp(pairwise ~ fraction | pool)     # and this will get expanded out to all pairwise
 ##   )
-## ),
+## )
+
+## model(
+##   design = ~ fraction * pool,                               # No replicates, but just imagine if there were
+##   comparisons = list(
+##     mult_comp(pairwise ~ fraction),                          # average across pools
+##     mult_comp(pairwise ~ fraction | pool),                   # stratify over pools
+##     mult_comp(trt.vs.ctrl ~fraction | pool, ref="tumour")    # All fraction_x vs tumour, per pool.  This would break on the 'no_cancer' dataset
+##     mult_comp(pairwise ~ fraction * pool, interaction=TRUE)  # which genes have pool-dependent response to fraction.  All pairwise interactions.
+##   )
+## )
+
