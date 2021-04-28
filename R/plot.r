1f50fb8b186a1961119c073471d803d829d0fa8f
Gavin Kelly <gavin.kelly@crick.ac.uk>
2021-04-28 09:35:55 +0100

    Fix syntax

--- R/plot.r
+++ R/plot.r
@@ -116,8 +116,8 @@ qc_heatmap <- function(dds, pc_x=1, pc_y=2, batch=~1, family="norm", title="QC V
                  hjust=1.1, vjust=-1.1, col="white", cex=6,
                  fontface = "bold", alpha = 0.8) + 
         geom_point(size=3) +
-        xlab(paste0("PC ", pc_x, ": ", pc_resid[[j]]$percent[,pc_x], "% variance")) +
-        ylab(paste0("PC ", pc_y, ": ", pc_resid[[j]]$percent[,pc_y], "% variance")) +
+        xlab(paste0("PC ", pc_x, ": ", pc_resid[[j]]$percent[pc_x], "% variance")) +
+        ylab(paste0("PC ", pc_y, ": ", pc_resid[[j]]$percent[pc_y], "% variance")) +
         labs(colour=j)
       if (do_labels) {pl <- pl + geom_text_repel(aes(label=sample))}
       print(pl)
